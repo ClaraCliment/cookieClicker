c480c8848e2175cda62c5beee8d69288a1e84a75
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-10-26 19:58:56 +0200

    back-end bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ let show = (score, btn, prix) => {
      * Increase start a setInterval to increase the click per seconds 
      * @param {Number} number - click per seconds
      */
-let increase = () => {
+let autoincrease = () => {
     setInterval(() => {
         cookieClicker.click()
         show(credit, btnMultiplier, coutMultiplier);
@@ -38,7 +38,7 @@ let bonus = () => {
         b--
         if (b == 0) finish();
         else {
-            click_bonus = (click * 2) - 1
+            cookieClicker.click()
         }
     }
     // let play = () => {
@@ -55,6 +55,43 @@ let isopen = () => {
     let menu = document.getElementById("menu_settings")
     if (menu.style.display == "none") return false
     else return true
+}
+
+let updateScore = () => {
+    labelMultiplier.innerHTML = "Multiclick " + coutMultiplier;
+    labelAutoClicker.innerHTML = "Autoclick " + coutAutoClick;
+    labelBonus.innerHTML = "Bonus " + coutBonus;
+    score.innerHTML = credit;
+    xMulti.innerHTML = "x" + multicounter
+    xAuto.innerHTML = "x" + autocounter
+    cps.innerHTML = clickSec + " click/sec "
+}
+let reset = () => {
+    btnMultiplier.disabled = "true"
+    btnAutoClicker.disabled = "true"
+    btnBonus.disabled = "true"
+    score.innerHTML = 0
+    cps = document.getElementById("clicks_seconde")
+    labelMultiplier.innerHTML = "Multiclick " + coutMultiplier;
+    labelAutoClicker.innerHTML = "Autoclick " + coutAutoClick;
+    labelBonus.innerHTML = "Bonus " + coutBonus;
+    timeleft.style.display = "none"
+    xMulti = document.getElementById('x_multi')
+    let a = document.querySelector("#auto-clicker .x_fois")
+    a.style.display = "none"
+    let b = document.querySelector("#multiplier .x_fois")
+    b.style.display = "none"
+    click_bonus = 0
+    credit = 0;
+    coutMultiplier = 30;
+    coutAutoClick = 120;
+    coutBonus = 250;
+    clickPower = 1;
+    clickSeconde = 1;
+    autocounter = 1
+    multicounter = 1
+    intervalId = null
+    b = 30
 }
 
 /*---------
@@ -75,61 +112,65 @@ const xMulti = document.getElementById('x_multi')
 const xAuto = document.getElementById('x_auto')
 const btnMenu = document.getElementById("settings")
 
-
 //Compteur
 var click_bonus = 0
 var credit = 0;
 var coutMultiplier = 30;
-var coutAutoClick = 120;
+var coutAutoClick = 12;
 var coutBonus = 250;
-var click = 1;
-var clickSeconde = 1;
+var clickPower = 1;
+var clickSec = 1;
 var intervalId = null
+var autocounter = 0
+var multicounter = 0
 var b = 30
 labelMultiplier.innerHTML = "Multiclick " + coutMultiplier;
 labelAutoClicker.innerHTML = "Autoclick " + coutAutoClick;
 labelBonus.innerHTML = "Bonus " + coutBonus;
+
 //EventListener
 cookieClicker.addEventListener("click", () => {
     // play()
-    credit = credit + click + click_bonus;
-    score.innerHTML = credit;
+    credit = credit + clickPower
+    updateScore()
     show(credit, btnMultiplier, coutMultiplier);
     show(credit, btnAutoClicker, coutAutoClick);
     show(credit, btnBonus, coutBonus)
 })
 btnMultiplier.addEventListener('click', () => {
+    clickSec = clickSec + multicounter;
+    multicounter++
+    credit = credit - coutMultiplier;
+    clickPower = clickPower + 1;
     let a = document.querySelector("#multiplier .x_fois")
     a.removeAttribute("style")
-    credit = credit - coutMultiplier;
-    click = click + 1;
     coutMultiplier = coutMultiplier * 2;
-    xMulti.innerHTML = "x" + click
-    score.innerHTML = credit;
+    updateScore()
     labelMultiplier.innerHTML = "Multiclick " + coutMultiplier;
     show(credit, btnMultiplier, coutMultiplier);
+    show(credit, btnAutoClicker, coutAutoClick);
+    show(credit, btnBonus, coutBonus)
 })
 btnAutoClicker.addEventListener('click', () => {
-    let a = document.querySelector("#auto-clicker .x_fois")
-    a.removeAttribute("style")
+    clickSec = clickSec + multicounter;
+    autocounter++
     credit = credit - coutAutoClick;
     coutAutoClick = coutAutoClick * 2;
-    increase();
+    autoincrease();
+    let a = document.querySelector("#auto-clicker .x_fois")
+    a.removeAttribute("style")
     cps.removeAttribute("style")
-    labelAutoClicker.innerHTML = "Autoclick " + coutAutoClick;
-    cps.innerHTML = clickSeconde + " click/sec "
-    clickSeconde++;
-    xAuto.innerHTML = "x" + clickSeconde
-
+    updateScore()
 })
 btnBonus.addEventListener("click", () => {
     credit = credit - coutBonus
     coutBonus = coutBonus * 4
-    score.innerHTML = credit;
     start()
     console.log("bonus activated")
     timeleft.removeAttribute("style")
-    labelBonus.innerHTML = "Bonus " + coutBonus;
+    updateScore()
+    show(credit, btnMultiplier, coutMultiplier);
+    show(credit, btnAutoClicker, coutAutoClick);
     show(credit, btnBonus, coutBonus)
 })
 
@@ -140,3 +181,4 @@ btnMenu.addEventListener("click", () => {
         console.log("open")
     } else { hide() }
 })
+reset()
